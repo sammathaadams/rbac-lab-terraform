9fb7e1ea0fb1e381ac7705304f6917b5f50ac94f
Samm <sammdion@gmail.com>
2026-03-21 16:39:03 -0400

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,15 @@
+##############################################################################
+# versions.tf
+# Purpose : Terraform version and provider requirements
+##############################################################################
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+  }
+}
